37cb4406bcc91429df51b0dd350c432807d02f56
Javier Fong <jfong@bewais.com>
2025-08-16 22:42:45 -0600

    llm utils file creation

---
+++ utils/llm.py
@@ -0,0 +1,56 @@
+import os 
+from pymongo import MongoClient
+from groq import AsyncGroq
+
+# Connect to MongoDB
+
+mongo_user = os.getenv('GENEZ_MONGO_DB_USER')
+mongo_psw = os.getenv('GENEZ_MONGO_DB_PSW')
+mongo_host = os.getenv('GENEZ_MONGO_DB_HOST')
+mongo_db = os.getenv('GENEZ_MONGO_DB_NAME')
+
+client = MongoClient(f"mongodb+srv://{mongo_user}:{mongo_psw}@{mongo_host}/?retryWrites=true&w=majority")
+db = client[mongo_db]
+
+ongoing_conversations = db["ongoing_conversations"]
+
+def handle_conversation(convo_id, sender_id, text):
+    # Look for the sender in the ongoing_conversations collection
+    conversation = ongoing_conversations.find_one({"sender_id": convo_id})
+    
+    if conversation:
+        # If the sender exists, add the text to the messages array
+        ongoing_conversations.update_one(
+            {"sender_id": convo_id},
+            {"$push": {"messages": {"sender": sender_id, "text": text}}}
+        )
+    else:
+        # If the sender doesn't exist, create a new document
+        new_conversation = {
+            "sender_id": convo_id,
+            "symptoms": [],
+            "location": {"lat": None, "lon": None, "text_description": None},
+            "language": None,
+            "messages": [{"sender": sender_id, "text": text}],
+            "recommendation": None
+        }
+        ongoing_conversations.insert_one(new_conversation)
+
+
+
+groq_api = os.getenv('GROQ_API_KEY')
+client = AsyncGroq(api_key = groq_api)
+
+async def get_completition(prompt): 
+    response = await client.chat.completions.create(
+        model = "llama-3.3-70b-versatile"
+        , messages = [
+            {"role" : "system", "content": "You are a recommendation engine of good medical professionals. Be concise and respectful."}
+            , {"role" : "user", "content": prompt}
+        ]
+        , temperature = 0.8 
+        , max_completion_tokens = 120
+        , top_p= 1 
+    )
+
+    return response.choices[0].message.content 
